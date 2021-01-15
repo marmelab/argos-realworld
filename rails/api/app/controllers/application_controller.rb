64c005daa76d168b491b3ba8e418b6bba56fa687
pem <pierre-etienne.moreau@loria.fr>
2021-01-15 17:00:09 +0100

    remove CSRF protection

--- rails/api/app/controllers/application_controller.rb
+++ rails/api/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+#   skip_before_action :verify_authenticity_token
+#   protect_from_forgery with: :null_session
 
   respond_to :json
 
