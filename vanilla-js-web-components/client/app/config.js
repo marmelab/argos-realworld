7490682a53cb4b9bc691948999227a17dfa31223
Pierre-Etienne Moreau <pemoreau.mail@gmail.com>
2021-03-02 11:43:26 +0100

    Update vanilla-js-web-components/client/app/config.js
    Co-authored-by: Clément Le Biez <clement@marmelab.com>

--- vanilla-js-web-components/client/app/config.js
+++ vanilla-js-web-components/client/app/config.js
@@ -1,6 +1,6 @@
 
-const dockerized = !(document.location.host.includes('localhost'));
-const API_ROOT = dockerized ? "http://api:4000/api" : "http://localhost:4000/api";
+const API_HOSTNAME = document.location.host.includes('localhost') ? 'localhost': 'api';
+const API_ROOT = `http://${API_HOSTNAME}:4000/api`;
 export const config = {
     rest_url : API_ROOT
     // rest_url: 'https://conduit.productionready.io/api/'
